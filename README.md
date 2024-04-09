ae795b740e91ee69b2abfb80f7fc9e1be4c09067
Otchi <adamgragnic@gmail.com>
2024-04-09 18:43:33 +0200

    Add base unreal

---
+++ README.md
@@ -0,0 +1,3 @@
+# LaBoulangerie
+
+Developed with Unreal Engine 5
---
+++ Source/LaBoulangerie/LaBoulangerie.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LaBoulangerie.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LaBoulangerie, "LaBoulangerie" );
